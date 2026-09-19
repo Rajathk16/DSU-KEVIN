00a4f1ef33403c2b50c425f9babd1e910c7936df
Gahan Shetty <gahanshetty16@gmail.com>
2026-09-19 05:56:49 +0530

    Updated env.js file

--- src/config/env.js
+++ src/config/env.js
@@ -17,16 +17,17 @@ for (const variable of requiredEnvVariables) {
 
 const env = {
   nodeEnv: process.env.NODE_ENV || "development",
-
   port: Number(process.env.PORT) || 5000,
-
   mongodbUri: process.env.MONGODB_URI,
-
   jwtSecret: process.env.JWT_SECRET,
-
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  clientUrl: process.env.CLIENT_URL || "http://localhost:5173",
 
-  clientUrl: process.env.CLIENT_URL || "http://localhost:5173"
+  emailHost: process.env.EMAIL_HOST,
+  emailPort: Number(process.env.EMAIL_PORT) || 587,
+  emailUser: process.env.EMAIL_USER,
+  emailPass: process.env.EMAIL_PASS,
+  emailFrom: process.env.EMAIL_FROM
 };
 
 module.exports = env;
